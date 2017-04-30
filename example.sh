02461d8e82c7cda8ea46f287c1ac61c504b20986
Roel Theeuwen <roel.theeuwen@gmail.com>
2017-04-30 15:51:46 +0200

    example

--- example.sh
+++ example.sh
@@ -3,7 +3,7 @@ clear
 mvn clean package assembly:assembly
 java -classpath ./target/test-classes:./target/apitogo-0.1-alpha-jar-with-dependencies.jar api.to.go.Example &
 JAVA_PID=$!
-sleep 1.2
+sleep 3
 
 curl -d "{\"id\":0,\"name\":\"Person A\",\"contacts\":[{\"id\":0,\"name\":\"Contact A\",\"phones\":[{\"id\":0,\"number\":123},{\"id\":0,\"number\":345}]},{\"id\":0,\"name\":\"ContactB\",\"phones\":[{\"id\":0,\"number\":567},{\"id\":0,\"number\":789}]}]}" localhost:8080/persons>/dev/null 2> /dev/null
 curl -d "{\"id\":0,\"name\":\"Person B\",\"contacts\":[{\"id\":0,\"name\":\"Contact C\",\"phones\":[{\"id\":0,\"number\":123},{\"id\":0,\"number\":345}]},{\"id\":0,\"name\":\"Contact D\",\"phones\":[{\"id\":0,\"number\":567},{\"id\":0,\"number\":789}]}]}" localhost:8080/persons>/dev/null 2> /dev/null
